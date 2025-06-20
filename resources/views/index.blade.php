255e235241b05744c3f701c0ee43b8e923b68630
shinobigua <136155207+shinobigua@users.noreply.github.com>
2025-06-20 10:21:58 +0700

    Revert "update beranda"
    This reverts commit e1831e9c8b7e146e4ef4953d334fb4052bd4f939.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,6 @@
 @section('content')
     @include('pages.post.edit')
     <div class="row">
-        mamamammaamamma
         <!-- Kolom Utama (Feed Postingan) -->
         <div class="col-8 ms-5">
             @foreach ($posts as $post)
